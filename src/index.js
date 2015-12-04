70ded911b62825faa67b97e7ac0b06935fa6af9d
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2015-12-04 11:11:17 +0100

    add angular example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // your awesome app goes here
+
+const app = angular.module('app', []);
+
+// @ngInject
+app.directive('greeting', ($timeout) => {
+  return {
+    restrict: 'E',
+    template: '<input ng-model="value"><span ng-click="click()">Csa {{name}} {{value}}!</span>',
+    link(scope, element, attr) {
+      scope.name = 'Tibi';
+      
+      scope.click = () => {
+        scope.name = 'Karcsi';
+        $timeout(() => {
+          scope.name = 'Katika';
+          alert(scope.value)
+        }, 2000)
+      }
+    }
+  };
+});
