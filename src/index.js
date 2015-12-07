e2a87d13884ac7fbe704ab8e43101e3bf2ee9c4d
Janos Hardi <janos.hardi@gawker.com>
2015-12-07 15:14:40 +0100

    admin screen initial commit

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ const app = angular.module('app', []);
 
 // @ngInject
 app.directive('greeting', ($timeout) => {
-  return {
-    restrict: 'E',
-    template: '<input ng-model="value"><span ng-click="click()">Csa {{name}} {{value}}!</span>',
-    link(scope, element, attr) {
-      scope.name = 'Tibi';
-      
-      scope.click = () => {
-        scope.name = 'Karcsi';
-        $timeout(() => {
-          scope.name = 'Katika';
-          alert(scope.value)
-        }, 2000)
-      }
-    }
-  };
+	return {
+		restrict: 'E',
+		template: '<input ng-model="value"><span ng-click="click()">Csa {{name}} {{value}}!</span>',
+		link(scope, element, attr) {
+			scope.name = 'Tibi';
+
+			scope.click = () => {
+				scope.name = 'Karcsi';
+				$timeout(() => {
+					scope.name = 'Katika';
+					window.alert(scope.value);
+				}, 2000);
+			};
+		}
+	};
 });
