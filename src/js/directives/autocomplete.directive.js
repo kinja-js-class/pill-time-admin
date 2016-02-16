6cf0efeb59944eee0c1894b07c0570d3143492e3
Laszlo Heves <laszlo.heves@gawker.com>
2016-02-16 14:50:13 +0100

    Move controller to directive

--- src/js/directives/autocomplete.directive.js
+++ src/js/directives/autocomplete.directive.js
@@ -1,13 +1,47 @@
-APP.directive('fetchData', () => {
+APP.directive('adminForm', (patientFctry) => {
 
-	let link = (scope, element, attrs) => {
-		element.bind('keyup', function () {
-			scope.checkComplete();
+	let link = (scope, element, attrs, controller) => {
+		element.bind('keypress', function () {
+			controller.checkComplete();
 		});
+	},
+
+	controller = function () {
+		let checkComplete, saveData, _isComplete;
+
+		checkComplete = () => {
+			console.log(this.isComplete);
+			this.isComplete = _isComplete();
+		};
+
+		saveData = () => {
+			let body = Object.assign({}, this.treatment);
+
+			delete body.patient;
+
+			patientFctry.save(this.treatment.patient, body);
+			this.treatment = {};
+		};
+
+		_isComplete = () => {
+			let treatment = this.treatment;
+			if ((Object.keys(treatment).length) === 3) {
+				return Object.keys(treatment).every (el => treatment[el] !== '');
+			} else {
+				return false;
+			}
+		};
+
+		this.treatment = {};
+		this.saveData = saveData;
+		this.checkComplete = checkComplete;
+		this.isComplete = false;
 	};
 
 	return {
 		restrict: 'A',
+		controller: controller,
+		controllerAs: 'admin',
 		link: link
 	};
 });
